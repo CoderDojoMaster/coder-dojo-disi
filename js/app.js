c841561d8459b4f476bf1a6f539217038f9d7e61
samdja <samuelgiacomelli@gmail.com>
2015-08-20 00:03:51 +0200

    Prima pagina quasi finita, forse, ma temo di no

---
+++ js/app.js
@@ -0,0 +1,30 @@
+/* modulo principale dell'applicazione con relative dipendenze */
+angular.module('coderdojo', ['ngRoute', 'homeModule'])
+
+
+/* routing e navigazione nelle pagine del sito */
+.config(['$routeProvider', function ($routeProvider) {
+
+    /* routing of the site */
+    $routeProvider.when('/', {
+        redirectTo: '/home'
+    }).when('/home', {
+        templateUrl: '../partials/home.html',
+        controller: 'homeController'
+    }).when ('/web/', {
+        templateUrl: '../partials/web.html',
+        controller: 'webController'
+    }).otherwise({
+        redirectTo: '/error'
+    });
+}])
+
+.directive('loading', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '../partials/directives/loading.html',
+        scope: {
+            loading: '=loading'
+        }
+    }
+})
