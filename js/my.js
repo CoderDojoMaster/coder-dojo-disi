891d5e6e26f9a707c047028b7d7373bcd8763d79
Michele <michele.romani.gzl0@me.com>
2015-09-17 22:55:21 +0200

    Pulizia pagina courses, riposizionamento del relativo css, prova di codice JS per le animazioni

--- js/my.js
+++ js/my.js
@@ -1,1 +1,21 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+var state = true;
+
+$(document).ready(function(){
+
+    $("h4").hover(function(){
+        $("h4").fadeOut();
+
+    });
+
+    $('.card').click(function(){
+        $('.card').fadeOut();
+
+    });
+
+});
+
 $(".button-collapse").sideNav();
+
