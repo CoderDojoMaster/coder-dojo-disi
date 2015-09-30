db4c2d9124ae3a5f0f5ea6e71a7b7924ef716e57
samdja <samuelgiacomelli@gmail.com>
2015-09-30 11:11:04 +0200

    Aggiunto feedback pagina attiva nel menu di navigazione

--- js/my.js
+++ js/my.js
@@ -1,30 +1,27 @@
-/*jslint browser: true*/
-/*global $, jQuery, alert*/
-
-
 $(document).ready(function () {
     $('.flipWrapper').click(function () {
         $(this).find('.flip-card').toggleClass('flipped');
         return false;
     });
+
+    $('.navbar-menu-item').click(function () {        
+        $('.navbar-menu-item').each( function () {
+        
+            $(this).removeClass("grey-text");
+            $(this).addClass("white-text");
+        });
+        
+        $(this).removeClass("white-text");
+        $(this).addClass("grey-text text-darken-1");
+        
+    });
+    
+    $('.coder-dojo-logo-a').click(function () {
+    
+        $('.navbar-menu-item').each( function () {
+        
+            $(this).removeClass("grey-text");
+            $(this).addClass("white-text");
+        });
+    });
 });
-
-
-
-/*
-$(document).ready(function(){
-
-    $("card").click(function(){
-        $("#card").flip();
-
-    });
-
-    $('.card').click(function(){
-        $('.card').fadeOut();
-
-    });
-
-});*/
-
-$(".button-collapse").sideNav();
-
