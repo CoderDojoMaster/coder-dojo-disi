d75e38991d174e32bcfbccd43e7e1c5dd78c400d
samdja <samuelgiacomelli@gmail.com>
2015-10-01 11:47:37 +0200

    Migliorato il feedback sul routing pagina

--- js/my.js
+++ js/my.js
@@ -4,23 +4,39 @@ $(document).ready(function () {
         return false;
     });
 
-    $('.navbar-menu-item').click(function () {        
-        $('.navbar-menu-item').each( function () {
-        
-            $(this).removeClass("grey-text");
+    $('.navbar-menu-item').click(function () {
+        $('.navbar-menu-item').each(function () {
+
+            $(this).removeClass("grey-text page-active");
             $(this).addClass("white-text");
         });
-        
+
         $(this).removeClass("white-text");
-        $(this).addClass("grey-text text-darken-1");
-        
+        $(this).addClass("grey-text text-darken-1 page-active");
+
     });
-    
+
+    $('.mobile-menu-link').click(function () {
+        var current = $(this).attr('href');
+
+        $('.navbar-menu-item').each(function () {
+
+            if (current == $(this).attr('href')) {
+                $(this).removeClass("white-text");
+                $(this).addClass("grey-text text-darken-1 page-active");
+            } else {
+                $(this).removeClass("grey-text page-active");
+                $(this).addClass("white-text");
+            }
+        });
+
+    });
+
     $('.coder-dojo-logo-a').click(function () {
-    
-        $('.navbar-menu-item').each( function () {
-        
-            $(this).removeClass("grey-text");
+
+        $('.navbar-menu-item').each(function () {
+
+            $(this).removeClass("grey-text page-active");
             $(this).addClass("white-text");
         });
     });
