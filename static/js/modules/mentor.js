ba8095c12f95b848d76656e70ef545b0be25d447
Andrea <andrea.zorzi.94@gmail.com>
2015-11-13 16:05:43 +0100

    Fix design pagina d'errore e template loading.

--- static/js/modules/mentor.js
+++ static/js/modules/mentor.js
@@ -4,53 +4,49 @@
     /* modulo per la comunicazione con il server dei dati relativi ai mentor */
     angular.module('mentorModule', ['ngResource'])
 
-        .factory('Mentor', ['$resource', function ($resource) {
+    .factory('Mentor', ['$resource', function ($resource) {
 
-            return $resource('/api/mentors/:mid', {}, {
-                query: {method:'GET', isArray:true}
-            });
-        }])
+        return $resource('/api/mentors/:mid', {}, {
+            query: {method:'GET', isArray:true}
+        });
+    }])
 
     .controller('aboutController', ['Mentor', function (Mentor) {
 
         var ctrl = this;
         this.mentors = [];
         this.loading = true;
-            this.page = 1;
+        this.page = 1;
 
         this.loadMentors = function (page) {
             Mentor.get({"max_results":"6","page":page},function (data) {
-                    //success
-                    console.log(data._items.length);
-                    if (data._items.length != 0) {
-                        for(var i = 0; i < data._items.length; i++) {
-                        ctrl.mentors.push(data._items[i]);
-                        console.log(data._items[i]);
-                    }
-                        ctrl.loadMentors(++ctrl.page);
-                    }
-                    ctrl.loading = false;
-                }, function (error) {
-                    //error
-                    ctrl.loading = false;
-                    ctrl.error = true;
-                    if (error.status == 404) {
-                        Materialize.toast("Content not found", 3000, 'red white-text');
-                    } else if (error.satus == 500) {
-                        Materialize.toast("Server error", 3000, 'red white-text');
-                    }
-                }, function () {
-                    //loading
+                //success
+                if (data._items.length !== 0) {
+                    ctrl.mentors = ctrl.mentors.concat(data._items);
+                    ctrl.loadMentors(++ctrl.page);
                 }
-            );
-        };
+                ctrl.loading = false;
+            }, function (error) {
+                //error
+                ctrl.loading = false;
+                ctrl.error = true;
+                if (error.status == 404) {
+                    Materialize.toast("Content not found", 3000, 'red white-text');
+                } else if (error.satus == 500) {
+                    Materialize.toast("Server error", 3000, 'red white-text');
+                }
+            }, function () {
+                //loading
+            }
+        );
+    };
 
-            this.loadMentors(ctrl.page);
+    this.loadMentors(ctrl.page);
 
 
-            clear_navigation();
+    clear_navigation();
 
-        $('#about-link').addClass("page-active");
-    }]);
+    $('#about-link').addClass("page-active");
+}]);
 
 })();
